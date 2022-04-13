ec8094ad754b390c69508985c29de4342ec19ed2
Gilmar <gilmar.jesus@gmail.com>
2022-04-13 20:56:18 -0300

    testado se o usuario e senha sao  invalidos.

--- aluraflix/tests/test_autentication.py
+++ aluraflix/tests/test_autentication.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate
 from django.urls import reverse
 from rest_framework import status
 
+
 class AuthenticationUserTesteCase(APITestCase):
 
     def setUp(self):
@@ -14,7 +15,7 @@ class AuthenticationUserTesteCase(APITestCase):
         '''
         Teste que verifica a autenticação de um user com credenciais corretas.
         '''
-        user = authenticate(username = 'c3po', password = '123456')
+        user = authenticate(username='c3po', password='123456')
         self.assertTrue((user is not None) and user.is_authenticated)
 
     def test_requisicao_get_nao_autorizada(self):
@@ -23,3 +24,17 @@ class AuthenticationUserTesteCase(APITestCase):
         '''
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_autenticacao_de_user_com_username_incorreto(self):
+        '''
+        Teste que verifica se o usuario e invalido.
+        '''
+        user = authenticate(username='c3pp', password='123456')
+        self.assertFalse((user is not None) and user.is_authenticated)
+
+    def test_autenticacao_de_user_com_password_incorreto(self):
+        '''
+        Teste que verifica se o usuario e invalido.
+        '''
+        user = authenticate(username='c3po', password='123455')
+        self.assertFalse((user is not None) and user.is_authenticated)
